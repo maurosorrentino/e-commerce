76df9a128fdc5b7579ab539b90a777ef12294a62
mauro <sorrentino.mauro95@gmail.com>
2021-05-10 15:06:52 +0100

    port

--- backend/app.js
+++ backend/app.js
@@ -184,7 +184,7 @@ mongoose.connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true,
 
     .then(() => {
 
-        server.listen(8090);
+        server.listen(8090 || 5000);
         console.log('connected to db');
 
     })
