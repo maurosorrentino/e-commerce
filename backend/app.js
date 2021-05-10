789f97a308e3ce7874bd9ec39da2bc935a8d034e
mauro <sorrentino.mauro95@gmail.com>
2021-05-10 15:42:48 +0100

    heroku

--- backend/app.js
+++ backend/app.js
@@ -184,7 +184,7 @@ mongoose.connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true,
 
     .then(() => {
 
-        server.listen(process.env.PORT || 5000);
+        server.listen(process.env.PORT, '0.0.0.0');
         console.log('connected to db');
 
     })
