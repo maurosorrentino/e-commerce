dd51055d22e6c2d1c8f8bd2e80baa7d25e13f351
mauro <sorrentino.mauro95@gmail.com>
2021-05-12 14:19:07 +0100

    production

--- backend/app.js
+++ backend/app.js
@@ -120,7 +120,7 @@ if(process.env.NODE_ENV === 'production') {
   
     app.get('*', (req, res) =>
 
-        res.sendFile(path.resolve(__dirname + '/../', 'frontend' , '.next', 'server', 'pages'))
+        res.sendFile(path.resolve(__dirname + '/../', 'frontend' , '.next', 'server', 'pages', 'index.html'))
 
     );
 
