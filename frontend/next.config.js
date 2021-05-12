1a2a70c4f5eafd63017c562ec2cf3136bdc65b9d
mauro <sorrentino.mauro95@gmail.com>
2021-05-12 09:52:11 +0100

    production

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,5 +4,6 @@ module.exports = {
         PRESET: process.env.PRESET,
         LOCALHOST: process.env.LOCALHOST,
         STRIPE: process.env.STRIPE,
+        NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
     },
   };
