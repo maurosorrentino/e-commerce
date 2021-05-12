f5d7d995bf641104a624059fca042f98ef8a43af
mauro <sorrentino.mauro95@gmail.com>
2021-05-12 12:47:32 +0100

    production

--- backend/app.js
+++ backend/app.js
@@ -118,15 +118,18 @@ if (process.env.NODE_ENV === 'production') {
     
     app.use(express.static(path.join(__dirname + '/../frontend/.next/server/pages')));
   
-    app.get('*', (req, res) =>
+    app.get('/', (req, res) =>
 
     res.sendFile(path.resolve(__dirname + '/../frontend/.next/server/pages/index.html'))
 
     );
 
   } else {
+
     app.get('/', (req, res) => {
+
       res.send('API is running....')
+
     })
   }
 
