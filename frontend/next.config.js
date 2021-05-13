35f357617b9d447b76e8761a48070f42c9c7ef10
mauro <sorrentino.mauro95@gmail.com>
2021-05-13 15:58:45 +0100

    production

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,4 +6,15 @@ module.exports = {
         STRIPE: process.env.STRIPE,
         NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
     },
+
+    exportPathMap: async function (
+        defaultPathMap,
+        { dev, dir, outDir, distDir, buildId }
+      ) {
+        return {
+          '/': { page: '/' },
+          '/auth/cart': { page: '/auth/cart' },
+        }
+      },
+    
   };
