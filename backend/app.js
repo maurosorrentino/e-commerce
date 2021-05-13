5f233c5cf5f51d59c1f4837e9cbf38aca07ebf48
mauro <sorrentino.mauro95@gmail.com>
2021-05-13 10:58:50 +0100

    production

--- backend/app.js
+++ backend/app.js
@@ -29,8 +29,7 @@ const app = express();
 
 if(process.env.NODE_ENV === 'production') {
     
-    // app.use(express.static(path.join(path.resolve() + '/../frontend/.next/server/pages')));
-    app.use(express.static(__dirname + '/../', 'frontend' , '.next', 'server', 'pages'));
+    app.use(express.static(path.join(path.resolve() + '/../frontend/.next/server/pages')));
   
     app.get('*', (req, res) =>
 
