0ca2c300812f08b17be555df68a3f65ba09d3e3a
mauro <sorrentino.mauro95@gmail.com>
2021-05-13 16:42:00 +0100

    production

--- backend/app.js
+++ backend/app.js
@@ -264,19 +264,19 @@ if(process.env.NODE_ENV === 'production') {
 
     app.get('/reset-password', (req, res) =>
     
-        res.sendFile(path.resolve(__dirname + '/../', 'frontend' , 'out', 'auth', 'reset-password.html'))
+        res.sendFile(path.resolve(__dirname + '/../', 'frontend' , 'out', 'reset-password.html'))
     
     );
 
     app.get('/view-item/:itemId', (req, res) =>
     
-        res.sendFile(path.resolve(__dirname + '/../', 'frontend' , 'out', 'auth', 'view-item[itemId].html'))
+        res.sendFile(path.resolve(__dirname + '/../', 'frontend' , 'out', 'view-item[itemId].html'))
     
     );
 
     app.get('/reset-password-form/:resetToken/:userId', (req, res) =>
     
-        res.sendFile(path.resolve(__dirname + '/../', 'frontend' , 'out', 'auth', 'reset-password-form[resetToken][userId].html'))
+        res.sendFile(path.resolve(__dirname + '/../', 'frontend' , 'out', 'reset-password-form[resetToken][userId].html'))
     
     );
 
